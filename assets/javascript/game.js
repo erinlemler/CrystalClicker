5788e8b1dc8a9327220214990e6983be536a05b2
Erin <erin_lemler@yahoo.com>
2018-04-30 22:04:01 -0700

    final product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 
-$(document).ready(function() {
+// $(document).ready(function() {
   var wins = 0; 
-  var target = Math.floor(Math.random()*60)+20
+  var target = Math.floor(Math.random()*60)+20;
   var losses = 0;
   var userScore = 0;
-  var pictures=["assets/images/arc.jpg","assets/images/arc.jpg","assets/images/arc.jpg","assets/images/arc.jpg"]
+  var pictures=["assets/images/Gem.png","assets/images/purple.png","assets/images/pink.ico","assets/images/pink__.png"]
 
-$("#userScore").text(userScore)
+$("#userScoreValue").text(userScore)
 $("#target").text(target)
 $("#userWins").text(wins)
 $("#userLoss").text(losses)
@@ -23,18 +23,27 @@ $(".clickCrystal").on("click",function(){
   var score=$(this).attr("data-crystal")
   userScore += parseInt(score)
 
-  $("#userScore").text(userScore)
-  
+  $("#userScoreValue").text(userScore)
+
   if (userScore > target){ //PLAYER LOST THE GAME
     alert("You lose!!")
     losses++; //this means increase this variable by 1
     $('#userLoss').text(losses);
+    userScore = 0;
+    $('#userScoreValue').text(userScore);
+    target = Math.floor(Math.random()*60)+20;
+    $("#target").text(target);
+
 } else if (userScore === target){ //PLAYER WON THE GAME
     alert("You win!!")
     wins++;
     $('#userWins').text(wins);
+    userScore = 0;
+    $('#userScoreValue').text(userScore);
+    target = Math.floor(Math.random()*60)+20;
+    $("#target").text(target);
 }
 })
 
-});
+// });
 
